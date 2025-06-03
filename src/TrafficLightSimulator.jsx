c183701633a90744070c365dbf81635783440c46
https://github.com/RabDino-Phulpoto <rabdinophulpoto@gmail.com>
2025-06-03 11:31:05 +0500

    Convert to fubctional react component

---
+++ src/TrafficLightSimulator.jsx
@@ -0,0 +1,55 @@
+import {useState} from 'react'
+import {useEffect} from 'react'
+import APP from './App.jsx'
+import { useTransition } from 'react';
+
+const[value,setvalue]=useState(0);
+void setvalue(value);
+
+function SetStyle()
+{
+    {useTransition.null}
+
+    this.TrafficLightSimulator(alet);
+    this.setvalue();
+    this.useEffect;
+}
+
+
+function Circle(){
+    return(
+        <div style="color:red , border:solid, border-ratio:50% ">
+            
+        </div>
+        {use.setvalue}
+        useDebugValue.SetStyle;
+    );
+}
+
+function Transitions()
+{
+    return(
+        <div>
+            <Transitions>{VideoColorSpace.caller}</Transitions>
+        </div>
+    );
+}
+
+export default function TrafficLightSimulator(){
+    return(
+        
+        <div style="size:30 , border:solid , background-color:brown">
+              <div>
+                       <Circle></Circle>
+                       <Transitions></Transitions>
+              </div>
+              <div>
+                       <Circle></Circle>
+              </div>
+              <div>
+                      <Circle></Circle>
+              </div>
+
+        </div>
+    );
+}
